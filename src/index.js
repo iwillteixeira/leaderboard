210910438a8cafc915e199093a232e91c27346bb
iwillteixeira <iwill2005@gmail.com>
2021-12-17 02:09:00 -0300

    project setup and plain html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './scss/style.scss';
+import { submitBtn } from './modules/domselector.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-
+  submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
 });
